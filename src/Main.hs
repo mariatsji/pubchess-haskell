07292db8d8137d86023744eb84b33fcc823e4d62
mariatsji <sjur.millidahl@gmail.com>
2014-08-15 14:15:35 +0200

    Something wrong in my map..

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main (
     main
 ) where
 
+import Control.Monad
 import Data.List
 import Data.Bits
 import Data.Maybe
@@ -9,7 +10,7 @@ import System.Environment
 import System.Directory
 import System.IO
 
-data Player = Player String Float deriving (Eq,Show)
+data Player = Player String deriving (Eq,Show)
 data Result = Unplayed | Remis | White | Black deriving (Eq,Show)
 data Match = Match Player Player Result deriving (Eq,Show)
 
@@ -25,18 +26,60 @@ single [] = []
 single (x:xs) = (combos x xs) ++ if (null xs)
                                     then []
                                     else single xs
-
 double :: [Player] -> [Match]
 double x = single x ++ map flipM (single x)
+
+players :: [String] -> [Player]
+players [] = []
+players x = map Player x
 
 twine :: [Match] -> [Match]
 twine x = map (\(a,b) -> if odd a then b else flipM b) (zip [1 .. (length x)] x)
 
 main = do
-    args <- getArgs
-    cmd <- getLine
-    process $ words cmd
+    putStrLn "Welcome. Whats the tournament called?"
+    tName <- getLine
+    putStrLn "single or double?"
+    single <- getLine
+    putStrLn  "What are the players names (use space)?"
+    playerN <- getLine
+    let ps = players $ words playerN
+        matches = createMatches single ps
+        matches' = zip [1 .. (length matches)] matches
+    process matches'
+    putStrLn "Good games!"
 
-process :: [String] -> IO ()
-process [] = "Unknown command"
-process _ = putStrLn "Processing .."
+process :: [(Int, Match)] -> IO ()
+process m = do
+    print' m
+    putStrLn "Enter result (e.g. '1 white', '2 remis', '3 black')"
+    cmd <- getLine
+    let m' = result cmd m
+    process m'
+
+
+result :: String -> [(Int, Match)] -> [(Int, Match)]
+result c [] = []
+result [] xs = []
+result c xs = map (\(i,x) ->
+                    if i == n
+                        then matchResult x r
+                        else x) xs
+            where   n = head $ words c
+                    r = head $ tail $ words c
+
+matchResult :: (Int, Match) -> String -> (Int, Match)
+matchResult (a, Match white black result) "white" = (a, Match white black White)
+matchResult (a, Match white black result) "black" = (a, Match white black Black)
+matchResult (a, Match white black result) "remis" = (a, Match white black Remis)
+
+
+print' :: [(Int, Match)] -> IO ()
+print' [] = putStrLn ""
+print' m = mapM_ print m
+
+createMatches :: String -> [Player] -> [Match]
+createMatches [] [] = []
+createMatches n [] = []
+createMatches "single" p = twine $ single p
+createMatches "double" p = double p
